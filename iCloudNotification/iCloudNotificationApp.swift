c85834cee3d910dc6e972107466ea63403e11ea6
HajarNashi <HajarNashi@gmail.com>
2022-12-18 18:25:56 +0300

    Initial Commit

---
+++ iCloudNotification/iCloudNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCloudNotificationApp.swift
+//  iCloudNotification
+//
+//  Created by Hajar Nashi on 18/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct iCloudNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
